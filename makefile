23c889d73e133a38172dba17f7ed340d5629ee17
Your Name <you@example.com>
2024-07-09 07:05:14 +0000

    init

---
+++ makefile
@@ -0,0 +1,90 @@
+# name for output binary files
+PROJECT ?= template
+
+# path to STM32F103 standard peripheral library
+CC = arm-none-eabi-gcc
+LD = arm-none-eabi-gcc
+OBJCOPY = arm-none-eabi-objcopy
+
+CC_PATH = /opt/gcc-arm-none-eabi/arm-none-eabi
+STD_PERIPH_LIBS ?= ./STM32F10x_StdPeriph_Lib_V3.6.0/
+
+SOURCES_DIR = src
+BUILD_DIR   = build
+INCLUDE_DIR = include
+STARTUP_DIR = startup
+LIB_DIR = lib
+3RD_LIB_DIR = 3rdparty
+
+# list of source files
+SOURCES  = $(SOURCES_DIR)/main.c
+SOURCES += $(SOURCES_DIR)/stm32f10x_it.c
+SOURCES += $(SOURCES_DIR)/system_stm32f10x.c
+SOURCES += $(STD_PERIPH_LIBS)/Libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_rcc.c
+# SOURCES += $(STD_PERIPH_LIBS)/Libraries/STM32F10x_StdPeriph_Driver/src/*.c
+SOURCES += $(STD_PERIPH_LIBS)/Libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_gpio.c
+SOURCES += $(STARTUP_DIR)/startup_stm32f10x_md.s
+SOURCES += $(STD_PERIPH_LIBS)/Libraries/CMSIS/CM3/CoreSupport/core_cm3.c
+
+INCLUDES  = -I$(INCLUDE_DIR)
+INCLUDES += -I$(STD_PERIPH_LIBS)/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/
+INCLUDES += -I$(STD_PERIPH_LIBS)/Libraries/CMSIS/CM3/CoreSupport/
+INCLUDES += -I$(STD_PERIPH_LIBS)/Libraries/STM32F10x_StdPeriph_Driver/inc/
+INCLUDES += -I/opt/gcc-arm-none-eabi/arm-none-eabi/include
+
+CFLAGS  = -g -Wall --specs=nosys.specs -march=armv7-m -O0
+CFLAGS += -mlittle-endian -mthumb -mcpu=cortex-m3 # -mthumb-interwork
+CFLAGS += -mfloat-abi=soft # -mfpu=fpv4-sp-d16 
+CFLAGS += -DSTM32F10X_MD -DUSE_STDPERIPH_DRIVER
+CFLAGS += -Wl,--gc-sections $(INCLUDES) 
+# CFLAGS += $(LIBS)
+
+LDFLAGS = -T$(STARTUP_DIR)/stm32_flash.ld
+# compiler, objcopy (should be in PATH)
+
+# path to st-flash (or should be specified in PATH)
+ST_FLASH ?= st-flash
+# ST_CLI ?= st-link
+# specify compiler flags
+
+OBJS = $(SOURCES:.c=.o)
+
+all: $(BUILD_DIR)/$(PROJECT).elf
+
+# compile
+$(BUILD_DIR)/$(PROJECT).elf: $(SOURCES)
+	@ if [ ! -d $(BUILD_DIR) ]; then mkdir $(BUILD_DIR); fi
+	@ $(CC) $(CFLAGS) $(LDFLAGS) $^ -o $@
+	$(OBJCOPY) -O ihex $(BUILD_DIR)/$(PROJECT).elf $(BUILD_DIR)/$(PROJECT).hex
+	$(OBJCOPY) -O binary $(BUILD_DIR)/$(PROJECT).elf $(BUILD_DIR)/$(PROJECT).bin
+
+# remove binary files
+.PHONY: clean flash compile_commands
+
+init_project:
+	@mkdir $(SOURCES_DIR); mkdir $(STARTUP_DIR); mkdir $(INCLUDE_DIR); mkdir $(LIB_DIR); mkdir $(3RD_LIB_DIR)
+	@cp $(STD_PERIPH_LIBS)/Project/STM32F10x_StdPeriph_Template/*.c $(SOURCES_DIR)
+	@cp $(STD_PERIPH_LIBS)/Project/STM32F10x_StdPeriph_Template/*.h $(INCLUDE_DIR)
+	@cp $(STD_PERIPH_LIBS)/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/startup/gcc_ride7/*.s $(STARTUP_DIR)
+	echo "you should cp the stm32_flash by yourself from $(STD_PERIPH_LIBS)/Project/STM32F10x_StdPeriph_Template/"
+
+
+clean:
+	@ if [ -d $(BUILD_DIR) ]; then rm -rf $(BUILD_DIR)/*.o $(BUILD_DIR)/*.elf $(BUILD_DIR)/*.hex $(BUILD_DIR)/*.bin; fi
+
+# flash
+flash:
+	# sudo $(ST_FLASH) write $(BUILD_DIR)/$(PROJECT).bin 0x8000000
+	/opt/openocd/bin/openocd -f /opt/openocd/openocd/scripts/interface/stlink-v2.cfg -f /opt/openocd/openocd/scripts/target/stm32f1x.cfg -c "program $(BUILD_DIR)/$(PROJECT).bin verify reset exit 0x08000000"
+
+compile_commands:
+	bear -- make all
+	mv compile_commands.json $(BUILD_DIR)/compile_commands.json
+# flash:
+# 	$(ST_CLI) -c SWD -P $(PROJECT).bin 0x8000000
+
+# read:
+# 	$(ST_CLI) -c SWD -Dump 0x0 0x8000000 $(PROJECT).bin
+
+# erase:
+# 	$(ST_CLI) -ME
---
+++ src/main.c
@@ -0,0 +1,192 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  MCD Application Team
+ * @version V3.6.0
+ * @date    20-September-2021
+ * @brief   Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2011 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "system_stm32f10x.h"
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef USE_STM32100B_EVAL
+#include "stm32100b_eval_lcd.h"
+#elif defined USE_STM3210B_EVAL
+#include "stm3210b_eval_lcd.h"
+#elif defined USE_STM3210E_EVAL
+#include "stm3210e_eval_lcd.h"
+#elif defined USE_STM3210C_EVAL
+#include "stm3210c_eval_lcd.h"
+#elif defined USE_STM32100E_EVAL
+#include "stm32100e_eval_lcd.h"
+#endif
+
+/** @addtogroup STM32F10x_StdPeriph_Template
+ * @{
+ */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#ifdef USE_STM32100B_EVAL
+#define MESSAGE1 "STM32 MD Value Line "
+#define MESSAGE2 " Device running on  "
+#define MESSAGE3 "  STM32100B-EVAL    "
+#elif defined(USE_STM3210B_EVAL)
+#define MESSAGE1 "STM32 Medium Density"
+#define MESSAGE2 " Device running on  "
+#define MESSAGE3 "   STM3210B-EVAL    "
+#elif defined(STM32F10X_XL) && defined(USE_STM3210E_EVAL)
+#define MESSAGE1 "  STM32 XL Density  "
+#define MESSAGE2 " Device running on  "
+#define MESSAGE3 "   STM3210E-EVAL    "
+#elif defined(USE_STM3210E_EVAL)
+#define MESSAGE1 " STM32 High Density "
+#define MESSAGE2 " Device running on  "
+#define MESSAGE3 "   STM3210E-EVAL    "
+#elif defined(USE_STM3210C_EVAL)
+#define MESSAGE1 " STM32 Connectivity "
+#define MESSAGE2 " Line Device running"
+#define MESSAGE3 " on STM3210C-EVAL   "
+#elif defined(USE_STM32100E_EVAL)
+#define MESSAGE1 "STM32 HD Value Line "
+#define MESSAGE2 " Device running on  "
+#define MESSAGE3 "  STM32100E-EVAL    "
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+#ifdef __GNUC__
+/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/* Private functions ---------------------------------------------------------*/
+static __IO uint32_t TimingDelay;
+
+/* Private function prototypes -----------------------------------------------*/
+void Delay(__IO uint32_t nTime);
+void System_Config(void);
+/**
+ * @brief  Main program.
+ * @param  None
+ * @retval None
+ */
+int main(void) {
+  /*!< At this stage the microcontroller clock setting is already configured,
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f10x_xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f10x.c file
+     */
+  SystemInit();
+  System_Config();
+  GPIO_InitTypeDef GPIO_InitStructure;
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  Delay(500);
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+  /* Infinite loop */
+  while (1) {
+    GPIO_ResetBits(GPIOE, GPIO_Pin_5);
+    GPIO_SetBits(GPIOB, GPIO_Pin_5);
+    Delay(500);
+    GPIO_ResetBits(GPIOB, GPIO_Pin_5);
+    GPIO_SetBits(GPIOE, GPIO_Pin_5);
+    Delay(500);
+  }
+}
+
+void Delay(__IO uint32_t nTime) {
+  TimingDelay = nTime;
+
+  while (TimingDelay != 0)
+    ;
+}
+
+void System_Config(void) {
+  if (SysTick_Config(SystemCoreClock / 1000)) {
+    // Handle Error
+    while (1)
+      ;
+  }
+}
+
+/**
+ * @brief  Decrements the TimingDelay variable.
+ * @param  None
+ * @retval None
+ */
+void TimingDelay_Decrement(void) {
+  if (TimingDelay != 0x00) {
+    TimingDelay--;
+  }
+}
+/**
+ * @brief  Retargets the C library printf function to the USART.
+ * @param  None
+ * @retval None
+ */
+PUTCHAR_PROTOTYPE {
+  // /* Place your implementation of fputc here */
+  // /* e.g. write a character to the USART */
+  // USART_SendData(USART1, (uint8_t) ch);
+
+  // /* Loop until the end of transmission */
+  // while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+  // {}
+
+  // return ch;
+  return 0;
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
+
+  /* Infinite loop */
+  while (1) {
+  }
+}
+#endif
+
+/**
+ * @}
+ */
